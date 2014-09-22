0e9f81351a88d2449fb85a657ed5e9ad63cada1b
Carlos Ramos <carlos.ramos1@alu.uclm.es>
2014-09-22 07:07:51 +0200

    Add version control

--- youtube_dl_simple_server/Initialization.py
+++ youtube_dl_simple_server/Initialization.py
@@ -11,23 +11,29 @@ from HttpServer import HttpServerHandler, ThreadedHTTPServer
 class Initialization ():
     def __init__(self, verbose):
         self._verbose = verbose
-        self.show('Initializing')
 
-        self.show('Checking for youtube-dl')
+        self.show('\nChecking for youtube-dl')
         if (not os.path.exists('/usr/local/bin/youtube-dl')):
             self.show('You need youtube-dl to catch links')
-            self.downloadYdl()
+            self.downloadYdlLatest()
 
-        self.show('youtube-dl --version\n' + subprocess.check_output(["youtube-dl", "--version"]))
+        self.show('\nChecking youtube-dl version')
+        version = subprocess.check_output(["youtube-dl", "--version"]).replace("\n", '')
+        latestVersion = self.getVersionYdlLatest()
+        self.show(' - Latest youtube-dl ' + latestVersion)
+        self.show(' - Yours  youtube-dl ' + version)
+        if version != latestVersion:
+            self.show(' You should update youtube-dl:')
+            self.show(' sudo youtube-dl -U')
 
         if (os.geteuid() == 0):
-            self.show('For safety, you can\'t start the server as root')
+            self.show('\nFor safety, you can\'t start the server as root. Run again')
         else:
-            self.show('Running HTTP server')
+            self.show('\nRunning HTTP server')
             httpd = ThreadedHTTPServer(('localhost', 49149), HttpServerHandler)
             httpd.serve_forever()
     
-    def downloadYdl (self):
+    def downloadYdlLatest (self):
         if (os.geteuid() != 0):
             self.show('You need root permss for auto-install. Try this:')
             self.show('sudo !!')
@@ -49,6 +55,13 @@ class Initialization ():
             self.show('\nRestart youtube-dl-simple-server')
             sys.exit(0)
 
+    def getVersionYdlLatest (self):
+        try:
+            latestVersion = urllib2.urlopen("https://yt-dl.org/latest/version").read()
+        except:
+            self.show('No Internet connexion')
+            sys.exit(1)
+        return latestVersion
 
     def show (self, s):
         if (self._verbose):
